d6ac12b560fc33d7d14f59cf4d8cbca97099c811
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-16 20:54:52 +0600

    adding all the think until outputing on the details page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import QuoteList from '../components/quotes/QuoteList';
+import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-    {id: 'q1' , author: 'shakil', text: 'Learing React is Fun!'}, 
-    {id: 'q2' , author: 'samia jannat', text: 'Learing React is Great!'}, 
-    
-]
+  { id: "q1", author: "shakil", text: "Learing React is Fun!" },
+  { id: "q2", author: "samia jannat", text: "Learing React is Great!" },
+];
 
 const AllQuotes = () => {
-    return (
-       <QuoteList quotes={DUMMY_QUOTES}/>
-    );
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuouteDetails.js
+++ src/pages/QuouteDetails.js
@@ -1,18 +1,31 @@
-import {Fragment} from 'react';
-import { useParams,Route } from 'react-router-dom';
-import Comments from '../components/comments/Comments';
+import { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "shakil", text: "Learing React is Fun!" },
+  { id: "q2", author: "samia jannat", text: "Learing React is Great!" },
+];
 
 const QuouteDetails = () => {
-  const params =  useParams()
-    return (
-        <Fragment>
-            <h1> quotes details page</h1>
-            <p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments/>
-            </Route>
-        </Fragment>
-    );
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuouteDetails;
