ff52d2b5509cb2d0ad1bfa67978593d3e80d1a56
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-16 18:37:35 +0600

    working in router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import QuouteDetails from "./pages/QuouteDetails";
@@ -7,6 +7,9 @@ import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
       {/* amara switch use korle must amara exact use korte hobe na hoy ata route move korbe na */}
       <Route path="/quotes" exact>
         <AllQuotes />
