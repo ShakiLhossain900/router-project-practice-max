9155ed7609add11d37ba748ced09f6250f0beda4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-16 19:27:10 +0600

    adding layout wrapper

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuouteDetails from "./pages/QuouteDetails";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      {/* amara switch use korle must amara exact use korte hobe na hoy ata route move korbe na */}
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuouteDetails />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        {/* amara switch use korle must amara exact use korte hobe na hoy ata route move korbe na */}
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuouteDetails />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/pages/QuouteDetails.js
+++ src/pages/QuouteDetails.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import {Fragment} from 'react';
+import { useParams,Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const QuouteDetails = () => {
+  const params =  useParams()
     return (
-        <div>
+        <Fragment>
             <h1> quotes details page</h1>
-            
-        </div>
+            <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments/>
+            </Route>
+        </Fragment>
     );
 };
 
