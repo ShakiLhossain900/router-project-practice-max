f91dec807c8b5de5680827289d5755f8d266d170
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-17 07:21:45 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AllQuotes from "./pages/AllQuotes";
 import QuouteDetails from "./pages/QuouteDetails";
 import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -22,7 +23,9 @@ function App() {
         <Route path="/new-quote">
           <NewQuote />
         </Route>
-        
+        <Route path="*">
+       <NotFound/>
+        </Route>
       </Switch>
     </Layout>
   );
