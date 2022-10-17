0a2f41366452cf73d7c33bbb92867fd77af0efd6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-17 07:32:55 +0600

    useHistory

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
+  //useHistory navigate korbe at the same  time ata navigate 
+  //howar pore amara kothay jete cacci submit korar por log y valueo jabe and ata page  change kore jei page er location debu oi page jabe
+const history = useHistory();
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push('/quotes')
+  };
 
-    const addQuoteHandler = quoteData =>{
-        console.log(quoteData);
-    }
-
-  return <QuoteForm onAddQuote={addQuoteHandler}/>
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
