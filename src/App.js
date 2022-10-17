8963f6475fec659976e3e1b2716d343b452681ab
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-17 07:17:38 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="/new-quote">
           <NewQuote />
         </Route>
+        
       </Switch>
     </Layout>
   );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="centered">
+            <p>Page is not found</p>
+        </div>
+    );
+};
+
+export default NotFound;
