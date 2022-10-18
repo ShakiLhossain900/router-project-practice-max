ae7de39b54479e69f6c2a50d5c9518f2fd46ec0c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-18 22:48:12 +0600

    get

--- src/pages/QuouteDetails.js
+++ src/pages/QuouteDetails.js
@@ -3,6 +3,7 @@ import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 import useHttp from "../hooks/use-http";
 import { getSingleQuote } from "../lib/api";
 
@@ -26,18 +27,22 @@ sendRequest(quoteId);
 },[sendRequest, quoteId])
 
 if(status === 'pending'){
-  
+  return <div className="centered">
+    <LoadingSpinner/>
+  </div>
 }
 
+if(error) {
+  return <p className='centered'> {error}</p>
+}
 
-
-  if (!quote) {
+  if (!loadedQuote) {
     return <p>No quote found!!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 
       {/* <Route path={`/quotes/${params.quoteId}`} exact> */}
       {/* changees */}
