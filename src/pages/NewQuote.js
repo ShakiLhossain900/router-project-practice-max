0f2c1283f7251a07fe91e567ec1dd27956ea283b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-18 21:12:36 +0600

    adding use http

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+
+import useHttp from '../hooks/use-http';
+
+
 const NewQuote = () => {
-  //useHistory navigate korbe at the same  time ata navigate 
+useHttp();
+
+  //useHistory navigate korbe at the same  time ata navigate
   //howar pore amara kothay jete cacci submit korar por log y valueo jabe and ata page  change kore jei page er location debu oi page jabe
-const history = useHistory();
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
-    history.push('/quotes')
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
