028aec38e118119bbbf12aeb9708603dd70850c8
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-18 22:15:13 +0600

    adding use http

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React,{useEffect} from "react";
 import QuoteList from "../components/quotes/QuoteList";
-
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "shakil", text: "Learing React is Fun!" },
@@ -8,6 +10,30 @@ const DUMMY_QUOTES = [
 ];
 
 const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true); //true control the loading state
+
+  useEffect(()=>{
+    sendRequest()
+  },[sendRequest]);
+
+
+  if(status === 'pending'){
+    return(
+      <div className='centered'>
+        <LoadingSpinner/>
+      </div>
+    )
+  }
+  if(error){
+    return <p className='centered focuses'>{error}</p>
+  }
+
+
   return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
