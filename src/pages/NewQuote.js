d6f9836ab5990c0967a73442b803baee5cb5344e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-18 21:28:43 +0600

    adding use http

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,35 +1,28 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
-import useHttp from '../hooks/use-http';
-import {addQuote} from '../lib/api';
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
-const {sendRequest, status}= useHttp(addQuote);
-const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+  };
 
-
-const addQuoteHandler = (quoteData) => {
-  sendRequest(quoteData)
-  history.push("/quotes");
-};
-
-
-
-return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
-
-
-
-
-
-
-
 
 //useHistory navigate korbe at the same  time ata navigate
 //howar pore amara kothay jete cacci submit korar por log y valueo jabe and ata page  change kore jei page er location debu oi page jabe
