a394a776d86e76eb6c5a589de9969c9d49ad9273
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-18 19:22:56 +0600

    adding hook and lib folder

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://router-practice-5-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
